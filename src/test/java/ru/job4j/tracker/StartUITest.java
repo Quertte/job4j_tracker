99852a2ce3e96f10a99b5871d996c627f6389094
Quertte <a.injen90@gmail.com>
2021-04-23 11:14:32 +0300

    5.1. Тестирование. Подготовка данных. [#507821]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -15,4 +15,26 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertEquals(created.getName(), expected.getName());
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "replaced item"};
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertEquals(replaced.getName(), "replaced item");
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Andrew");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId())};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
+    }
 }

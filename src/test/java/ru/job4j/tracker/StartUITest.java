91779f8b6e984af9705aac5403655907c2d23ab8
Quertte <a.injen90@gmail.com>
2021-04-28 07:55:28 +0300

    1.1. Тесты на StartUI. [#507844]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -227,4 +227,22 @@ public class StartUITest {
                         + "0. Find item by id" + System.lineSeparator()
                         + "1. Exit Program" + System.lineSeparator());
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"7", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{new ExitAction()};
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertEquals(out.toString(),
+                "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 ... 0" + ln
+                        + "Menu:" + ln
+                        + "0. Exit Program" + ln);
+    }
 }
